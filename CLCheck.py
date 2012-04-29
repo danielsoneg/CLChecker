011766cbeae4a74be16b10ea2e43940733c11e19
danielsoneg <git@egd.im>
2012-04-29 15:59:50 -0700

    Handle Spam links, relative paths

--- CLCheck.py
+++ CLCheck.py
@@ -1,9 +1,13 @@
-#!/usr/bin/env python3
+#!/usr/bin/env python
 import requests
 import random
 import sys
+import json
+import os
 from bs4 import BeautifulSoup
 
+path = os.path.dirname(os.path.realpath(__file__))
+lastfile = os.path.join(path,'last')
 def format_push(p):
     desc = p.text[p.text.find('-') + 2:p.text.rfind('-')]
     href = p.find('a').get('href')
@@ -18,18 +22,20 @@ def send_push(push):
     url = "http://boxcar.io/devices/providers/X5RCfRKbUoblfOfcbAMH/notifications"
     r = requests.post(url, data=push)
 
-with open('last') as w: last = w.read()
+with open(lastfile) as r: last = json.load(r)
 hi = random.randint(1700,1705)#VOODOO!
 lo = random.randint(1095,1100)#VOODOO!
 r = requests.get("http://sfbay.craigslist.org/search/apa/eby?query=&srchType=A&minAsk=%i&maxAsk=%i&bedrooms=2&nh=48&nh=49" % (lo,hi))
 #x = etree.parse(r.content, etree.HTMLParser())
 soup = BeautifulSoup(r.content)
-newlast = soup.find('p').find('a').get('href')
-if last == newlast:
-    sys.exit()
-with open('last','w') as w: w.write(newlast)
-for p in soup.findAll('p'):
+newlast = []
+for p in soup.findAll('p')[:10]: # last 10 only.
     a = p.find('a')
-    if a.get('href') == last: break
+    if a.get('href') in last: break
     push = format_push(p)
     send_push(push)
+    if len(newlast) < 5:
+        newlast.append(a.get('href'))
+newlast.extend(last)
+newlast = newlast[:5]
+with open(lastfile,'w') as w: json.dump(newlast,w)
